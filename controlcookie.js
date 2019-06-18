874e7d4565dd6b28c264b3ef822458d673fe1df3
ian <chu2815@hotmail.com>
2019-06-18 17:34:37 +0800

    Update controlcookie.js

--- controlcookie.js
+++ controlcookie.js
@@ -1,5 +1,4 @@
-var domainName = window.location.hostname;
-console.log(domainName)
+var domainName = 'iandays.com';
 document.cookie = "key=value; expires=Tue, 16 Dec 2019 11:31:56 GMT; path=/; domain=" + domainName;
 
 console.log(document.cookie)
