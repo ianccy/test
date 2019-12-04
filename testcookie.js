85db4b48cf4efe2721d6785aefa850d30edb6051
ian <chu2815@hotmail.com>
2019-12-04 10:18:18 +0800

    Update testcookie.js

--- testcookie.js
+++ testcookie.js
@@ -1,4 +1,4 @@
-var domainName = 'iandays.com';
-document.cookie = "key=value; expires=Tue, 16 Dec 2019 11:31:56 GMT; path=/; domain=" + domainName;
-
-console.log(document.cookie)
+var domainName = document.location.host;
+document.cookie = "key=value; expires=Tue, 16 Dec 2019 11:31:56 GMT; path=/; domain=." + domainName;
+console.log(domainName);
+console.log(document.cookie);
