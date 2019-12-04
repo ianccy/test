bd4708c82e3045b239979f5f136e752fc913490a
ian <chu2815@hotmail.com>
2019-12-04 10:18:07 +0800

    Delete setothercookie.js

--- setothercookie.js
+++
@@ -1,4 +0,0 @@
-var domainName = document.location.host;
-document.cookie = "key=value; expires=Tue, 16 Dec 2019 11:31:56 GMT; path=/; domain=." + domainName;
-console.log(domainName);
-console.log(document.cookie);
