1091df3489ace89b1c7df629b723af16bb3b0617
ian <chu2815@hotmail.com>
2019-12-04 10:16:41 +0800

    Update setothercookie.js

--- setothercookie.js
+++ setothercookie.js
@@ -1,4 +1,4 @@
-var domainName = 'cdpn.io';
+var domainName = document.location.host;
 document.cookie = "key=value; expires=Tue, 16 Dec 2019 11:31:56 GMT; path=/; domain=." + domainName;
-
-console.log(document.cookie)
+console.log(domainName);
+console.log(document.cookie);
